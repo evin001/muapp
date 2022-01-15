6827f920711290c7313ec3085f6cc34c2acf1b0b
Evgeniy <e19a@yandex.ru>
2022-01-15 21:19:53 +0300

    schedule edit form

--- packages/client/src/pages/Master/Schedule/Edit.tsx
+++ packages/client/src/pages/Master/Schedule/Edit.tsx
@@ -12,8 +12,11 @@ import {
   useTheme,
 } from '@stage-ui/core'
 import SelectTypes from '@stage-ui/core/control/Select/types'
-import { ArrowLeft } from '@stage-ui/icons'
+import { ArrowLeft, Save, Plus } from '@stage-ui/icons'
 import moment from 'moment'
+import { useForm, Controller } from 'react-hook-form'
+import { yupResolver } from '@hookform/resolvers/yup'
+import * as yup from 'yup'
 
 import { useMasterContext } from '..'
 
@@ -25,6 +28,15 @@ import { EnititiesActions } from '~/data/enitities'
 import { selectServicesByParentCategory } from '~/data/enitities/select'
 import { Page } from '~/components/Page'
 import { HintError } from '~/components/HintError'
+
+type EditFormType = {
+  intervalStart: string
+  intervalEnd: string
+  color: string
+  type: ScheduleEventType
+  date: Date
+  services?: string[]
+}
 
 const colorSaturation = 500
 const colorOptions = [
@@ -38,6 +50,14 @@ const colorOptions = [
   { text: 'Красный', value: 'red' },
 ]
 
+const schema = yup.object({
+  date: yup.string().required('Пожалуйста, укажите дату'),
+  intervalStart: yup.string().required('Пожалуйста, временной интервал'),
+  intervalEnd: yup.string().required('Пожалуйста, временной интервал'),
+  color: yup.string().required('Пожалуйста, укажите цвет'),
+  type: yup.string().required('Пожалуйста, укажите повторение'),
+})
+
 export const MasterScheduleEdit = () => {
   const { id } = useParams<{ id?: string }>()
   const title = id ? 'Редактирование события' : 'Новое событие'
@@ -49,6 +69,23 @@ export const MasterScheduleEdit = () => {
   const { setMenu } = useMasterContext()
 
   const servicesWithCategories = useSelector(selectServicesByParentCategory)
+
+  const {
+    handleSubmit,
+    control,
+    formState: { isValid },
+  } = useForm<EditFormType>({
+    defaultValues: {
+      date: new Date(),
+      intervalStart: '09:00',
+      intervalEnd: '10:00',
+      type: ScheduleEventType.Once,
+      color: 'purple',
+      services: [],
+    },
+    mode: 'onChange',
+    resolver: yupResolver(schema),
+  })
 
   useEffect(() => {
     setMenu('schedule')
@@ -72,6 +109,7 @@ export const MasterScheduleEdit = () => {
 
   const serviceOptions = useMemo(() => {
     return servicesWithCategories.map((serviceOrCategory) => {
+      if (!serviceOrCategory) return { text: '', value: '' }
       if ((serviceOrCategory as Service).category) {
         return {
           text: (serviceOrCategory as Service).category.name,
@@ -81,6 +119,8 @@ export const MasterScheduleEdit = () => {
       return { text: (serviceOrCategory as Category).name, value: 'group' }
     })
   }, [servicesWithCategories.length])
+
+  const handleSubmitForm = (data: EditFormType) => {}
 
   return (
     <Page
@@ -97,22 +137,40 @@ export const MasterScheduleEdit = () => {
         </Button>
       }
     >
-      <form>
+      <form onSubmit={handleSubmit(handleSubmitForm)}>
         <Grid gap="0.25rem">
-          <DatePicker
-            label="Дата"
-            placeholder="Выберите дату"
-            format="DD.MM.YYYY"
-            hint={<HintError error="" />}
+          <Controller
+            name="date"
+            control={control}
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <DatePicker
+                label="Дата"
+                placeholder="Выберите дату"
+                format="DD.MM.YYYY"
+                value={value}
+                onChange={(_, date) => {
+                  onChange(date)
+                }}
+                hint={<HintError error={error?.message} />}
+              />
+            )}
           />
           <Flexbox column>
             <Text size="s" mb="0.25rem">
               Временной интеравал
             </Text>
             <Grid alignItems="center" templateColumns="1fr 1.25rem 1fr" gap="0.625rem">
-              <TextField
-                placeholder="00:00"
-                pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"
+              <Controller
+                name="intervalStart"
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    placeholder="00:00"
+                    pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
               />
               <Flexbox
                 w="100%"
@@ -120,58 +178,108 @@ export const MasterScheduleEdit = () => {
                 backgroundColor="onBackground"
                 borderRadius="0.3125rem"
               />
-              <TextField
-                placeholder="00:00"
-                pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"
+              <Controller
+                name="intervalEnd"
+                control={control}
+                render={({ field: { value, onChange } }) => (
+                  <TextField
+                    placeholder="00:00"
+                    pattern="^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$"
+                    value={value}
+                    onChange={onChange}
+                  />
+                )}
               />
             </Grid>
             <HintError error="" />
           </Flexbox>
-          <Select
-            clearable
-            label="Повторение"
-            placeholder="Не повторять"
-            options={repetitionOptions}
-            hint={<HintError error="" />}
+          <Controller
+            name="type"
+            control={control}
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Select
+                label="Повторение"
+                options={repetitionOptions}
+                values={repetitionOptions.filter((t) => t.value === value)}
+                onChange={(_, option) => {
+                  onChange(option?.value)
+                }}
+                hint={<HintError error={error?.message} />}
+              />
+            )}
           />
-          <Select
-            clearable
-            label="Услуга"
-            placeholder="Любая услуга"
-            options={serviceOptions}
-            hint={<HintError error="" />}
-            renderOption={(option) => (
-              <Flexbox>
-                {option.value === 'group' ? (
-                  <Text css={{ fontFamily: font.medium }}>{option.text}</Text>
-                ) : (
-                  <Text ml="m">{option.text}</Text>
+          <Controller
+            name="services"
+            control={control}
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Select
+                multiselect
+                label="Услуга"
+                placeholder="Любая услуга"
+                options={serviceOptions}
+                values={serviceOptions.filter((s) => value?.includes(s.value))}
+                hint={<HintError error={error?.message} />}
+                onChange={(_, option) => {
+                  if (option?.value && option?.value !== 'group') {
+                    if (value?.includes(option.value.toString())) {
+                      onChange(value.filter((v) => v !== option.value))
+                    } else {
+                      onChange(value?.concat(option.value.toString()))
+                    }
+                  }
+                }}
+                renderOption={(option) => (
+                  <Flexbox>
+                    {option.value === 'group' ? (
+                      <Text css={{ fontFamily: font.medium }}>{option.text}</Text>
+                    ) : (
+                      <Text ml="m">{option.text}</Text>
+                    )}
+                  </Flexbox>
                 )}
-              </Flexbox>
+              />
             )}
           />
-          <Select
-            label="Цвет"
-            placeholder="Выберите цвет"
-            options={colorOptions}
-            renderOption={(option) => (
-              <Flexbox alignItems="center">
-                <Flexbox
-                  mr="s"
-                  w="2rem"
-                  h="1rem"
-                  backgroundColor={color.palette[
-                    `${option.value.toString()}${colorSaturation}`
-                  ].hex()}
-                  borderRadius="0.3125rem"
-                  borderWidth="0.0625rem"
-                  borderStyle="solid"
-                  borderColor="content3"
-                />
-                <Text>{option.text}</Text>
-              </Flexbox>
+          <Controller
+            name="color"
+            control={control}
+            render={({ field: { value, onChange }, fieldState: { error } }) => (
+              <Select
+                label="Цвет"
+                placeholder="Выберите цвет"
+                options={colorOptions}
+                values={colorOptions.filter((c) => c.value === value)}
+                onChange={(_, option) => {
+                  onChange(option?.value)
+                }}
+                renderOption={(option) => (
+                  <Flexbox alignItems="center">
+                    <Flexbox
+                      mr="s"
+                      w="2rem"
+                      h="1rem"
+                      backgroundColor={color.palette[
+                        `${option.value.toString()}${colorSaturation}`
+                      ].hex()}
+                      borderRadius="0.3125rem"
+                      borderWidth="0.0625rem"
+                      borderStyle="solid"
+                      borderColor="content3"
+                    />
+                    <Text>{option.text}</Text>
+                  </Flexbox>
+                )}
+                hint={<HintError error={error?.message} />}
+              />
             )}
-            hint={<HintError error="" />}
+          />
+
+          <Button
+            textColor="surface"
+            label={id ? 'Сохранить' : 'Добавить'}
+            leftChild={id ? <Save /> : <Plus />}
+            type="submit"
+            disabled={!isValid}
           />
         </Grid>
       </form>
